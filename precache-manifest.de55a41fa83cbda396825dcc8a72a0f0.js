dc5aeb927540d23a8b4f59a1596b80f10f3ac47f
tsah20 <tanyasah20@gmail.com>
2020-07-06 09:23:29 -0400

    Updates

---
+++ precache-manifest.de55a41fa83cbda396825dcc8a72a0f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631136789b4a38a893595ca0a04b8310",
+    "url": "/material_dashbaord/index.html"
+  },
+  {
+    "revision": "2bb21b0d7e1e42a9cb94",
+    "url": "/material_dashbaord/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "7f6e3a69e19c3561c8cb",
+    "url": "/material_dashbaord/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2bb21b0d7e1e42a9cb94",
+    "url": "/material_dashbaord/static/js/2.cc5a7b1e.chunk.js"
+  },
+  {
+    "revision": "d8ceb73620e9e7bd9ee590a182770f05",
+    "url": "/material_dashbaord/static/js/2.cc5a7b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f6e3a69e19c3561c8cb",
+    "url": "/material_dashbaord/static/js/main.510acf60.chunk.js"
+  },
+  {
+    "revision": "70103fabe672bb0b3544",
+    "url": "/material_dashbaord/static/js/runtime-main.2c918da0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material_dashbaord/static/media/logo.5d5d9eef.svg"
+  }
+]);
